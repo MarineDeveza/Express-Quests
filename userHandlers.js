5a2182a1327fd9fb381e51961182f31fcbed4171
MarineDeveza <marine.deveza@gmail.com>
2023-05-28 16:55:35 +0200

    solution

---
+++ userHandlers.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const database = require("./database");
+
+
+/*const getUsers = (req, res) => {
+  res.json(users);
+};*/
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+/*
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find((user) => user.id === id);
+
+  if (user != null) {
+    res.json(user);
+  } else {
+    res.status(404).send("Not Found");
+  }
+};
+*/
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+  };
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
